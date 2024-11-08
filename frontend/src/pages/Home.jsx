d05f7ce1d139bdd427f2d35fbad3de484bb70f88
ayan878 <ayanraza197@gmail.com>
2024-11-09 03:59:11 +0530

    home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,10 +5,12 @@ import Hero from "../components/Hero";
 import OurServices from "../components/OurServices";
 import WorkingProcess from "../components/WorkingProcess";
 // import Services from "./Services";
+import Header2 from "../components/Header2"
 
 function Home() {
   return (
     <div>
+      <Header2/>
       <Header />
       <Hero/>
       <BrandSlider/>
