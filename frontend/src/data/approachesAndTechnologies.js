18006cc3ccc4e11ac6ee86a0345cb3ea394e7874
ayan878 <ayanraza197@gmail.com>
2024-11-08 23:37:14 +0530

    data

---
+++ frontend/src/data/approachesAndTechnologies.js
@@ -0,0 +1,154 @@
+// Import all icons
+
+// Shapes
+// import Shape from "../assets/images/shapes/shape_space_3.svg?react";
+// import ShapeLine2 from "../assets/images/shapes/shape_line_2.svg?react";
+// import ShapeLine3 from "../assets/images/shapes/shape_line_3.svg?react";
+// import TitleUnderLine from "../assets/images/shapes/shape_title_under_line.svg?react";
+// import hexagon from "../assets/images/shapes/shape_polygon.webp";
+
+// Web Platform Icons
+import TypeScript from "../assets/images/icons/icon_typescript.svg";
+import Python from "../assets/images/icons/icon_python.svg";
+import javaScript from "../assets/images/icons/icon_javascript.svg";
+import React from "../assets/images/icons/icon_react_js.svg";
+import ReactNative from "../assets/images/icons/icon_react_native.svg";
+import Java from "../assets/images/icons/icon_java.svg";
+import Laravel from "../assets/images/icons/icon_laravel.svg";
+import Php from "../assets/images/icons/icon_php.svg";
+import Ruby from "../assets/images/icons/icon_ruby.svg";
+import C from "../assets/images/icons/icon_c.svg";
+import CPlus from "../assets/images/icons/icon_c_plus.svg";
+import Swift from "../assets/images/icons/icon_swift.svg";
+import Gatsby from "../assets/images/icons/icon_gatsby.svg";
+
+// Database Icons
+import PostgreSQL from "../assets/images/icons/icon_postgresql.svg"; 
+import MongoDB from "../assets/images/icons/icon_mongodb.svg";
+import OracleDB from "../assets/images/icons/icons_oracledb.svg";
+import MySQL from "../assets/images/icons/icon_mySql.svg";
+import Firestore from "../assets/images/icons/icon_firebase.svg";
+import Redis from "../assets/images/icons/icon_redis-.svg";
+import MSSQL from "../assets/images/icons/icon_microsoft-sql-server.svg";
+import Elasticsearch from "../assets/images/icons/icon_elasticsearch.svg";
+import ScyllaDB from "../assets/images/icons/icon_scylladb.svg";
+import BigchainDB from "../assets/images/icons/icon_bigchainDB.svg";
+
+// Cloud and DevOps Icons
+import Docker from "../assets/images/icons/icon_docker.svg"; 
+import Jenkins from "../assets/images/icons/icon_jenkins.svg";
+import Ansible from "../assets/images/icons/icon_ansible.svg";
+import Kubernetes from "../assets/images/icons/icon_kubernetes.svg";
+import AWS from "../assets/images/icons/icon-amazonWebServices.svg"
+import Terraform from "../assets/images/icons/icon_terraform.svg";
+import Jira from "../assets/images/icons/icon_jira.svg";
+import GitHub from "../assets/images/icons/icon_github.svg";
+
+
+// Mobile App Icons
+import Flutter from "../assets/images/icons/icon_flutter.svg";
+import Ionic from "../assets/images/icons/icon_ionic.svg";
+import Kotlin from "../assets/images/icons/icon_kotlin.svg";
+import Xamarin from "../assets/images/icons/icon_xamarin.svg";
+import AngularUI from "../assets/images/icons/icon_angular.svg";
+
+// Other Framework Icons
+import Django from "../assets/images/icons/icon_django.svg";
+import RubyOnRails from "../assets/images/icons/icon_rails.svg";
+import ExpressJS from "../assets/images/icons/icon_expressjs.svg";
+import DotNetCore from "../assets/images/icons/icon_netcore.svg";
+import CodeIgniter from "../assets/images/icons/icon_codeigniter.svg";
+import VueJS from "../assets/images/icons/vuejs.svg";
+import NextJS from "../assets/images/icons/icon_nextjs.svg";
+
+// Approaches Data
+export const approaches = [
+  {
+    title: "01. COMMUNICATION",
+    desc: "Once we have a contract in place, we open a connection on Slack or any other chatting tool...",
+  },
+  {
+    title: "02. PLANNING",
+    desc: "Within maximum 2 weeks since the start of the contract through the first planning meetings...",
+  },
+  {
+    title: "03. Execution",
+    desc: "Every quarter we design a new roadmap together for a bigger shape overview...",
+  },
+  {
+    title: "04. ARCHITECTURES AND INFRASTRUCTURES",
+    desc: "At the project start developers create the boilerplate and decide the tech stack...",
+  },
+  {
+    title: "05. TRANSPARENCY",
+    desc: "Everything from communication channels to tools we use is meant to make your life easier...",
+  },
+  {
+    title: "06. DELIVERY",
+    desc: "Every iteration has a 2 weeks duration, at the end of each, we deliver a product increase...",
+  },
+  {
+    title: "07. RETROSPECTIVE",
+    desc: "Every month we look back at our work, open to your suggestions on how to tweak the workflow...",
+  },
+];
+
+// Technologies Data
+export const technologies = {
+  WebPlatform: [
+    { name: "React JS", icon: React },
+    { name: "Next JS", icon: NextJS },
+    { name: "JavaScript", icon: javaScript },
+    { name: "Swift", icon: Swift },
+    { name: "TypeScript", icon: TypeScript },
+    { name: "Python", icon: Python },
+    { name: "Gatsby", icon: Gatsby },
+    { name: "Java", icon: Java },
+    { name: "Ruby", icon: Ruby },
+    { name: "C++", icon: CPlus },
+    { name: "C", icon: C },
+    { name: "PHP", icon: Php },
+    { name: "Laravel", icon: Laravel },
+    { name: "Laravel", icon: Python },
+  ],
+  Database: [
+    { name: "PostgreSQL", icon: PostgreSQL },
+    { name: "MongoDB", icon: MongoDB },
+    { name: "Oracle DB", icon: OracleDB },
+    { name: "MySQL", icon: MySQL },
+    { name: "Firestore", icon: Firestore },
+    { name: "Redis", icon: Redis },
+    { name: "Microsoft SQL", icon: MSSQL },
+    { name: "Elasticsearch", icon: Elasticsearch },
+    { name: "ScyllaDB", icon: ScyllaDB },
+    { name: "BigchainDB", icon: BigchainDB },
+  ],
+  CloudAndDevOps: [
+    { name: "Docker", icon: Docker },
+    { name: "Jenkins", icon: Jenkins },
+    { name: "Ansible", icon: Ansible },
+    { name: "Kubernetes", icon: Kubernetes },
+    { name: "AWS", icon: AWS },
+    { name: "Terraform", icon: Terraform },
+    { name: "Jira", icon: Jira },
+    { name: "Github", icon: GitHub },
+  ],
+  MobileApps: [
+    { name: "React Native", icon: ReactNative },
+    { name: "Flutter", icon: Flutter },
+    { name: "Ionic", icon: Ionic },
+    { name: "Java", icon: Java },
+    { name: "Kotlin", icon: Kotlin },
+    { name: "Xamarin", icon: Xamarin },
+    { name: "Swift", icon: Swift },
+    { name: "Angular UI", icon: AngularUI },
+  ],
+  OtherFrameworks: [
+    { name: "Django", icon: Django },
+    { name: "Ruby on Rails", icon: RubyOnRails },
+    { name: "Express.js", icon: ExpressJS },
+    { name: ".NET Core", icon: DotNetCore },
+    { name: "CodeIgniter", icon: CodeIgniter },
+    { name: "Vue.js", icon: VueJS },
+  ],
+};
